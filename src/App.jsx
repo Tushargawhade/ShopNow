aabfeabc26abfafa8d32054d101dedaee3cb09fe
Tusshar <tushargawhade7@gmail.com>
2025-12-18 01:12:22 +0530

    Navbar Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
 
 function App() {
   return (
-    <div className='w-screen h-screen bg-zinc-400'>
-      
+    <div className="w-screen h-screen flex">
+      <nav className="h-full w-[15%] bg-zinc-100 p-2 flex flex-col items-center   ">
+        <button className="px-3 py-1 border border-blue-300 text-blue-400 rounded mt-5 ">
+          Add New Product
+        </button>
+
+        <hr className="w-[80%] my-4" />
+
+        <h1 className="w-[80%] text-2xl  font-semibold  ">Category</h1>
+
+        <ul className="w-[80%] mt-3">
+          <li className="flex items-center mb-2">
+            <span className="w-4.5 h-4.5 rounded-full bg-blue-200 mr-2"></span>
+            cat 1
+          </li>
+
+          <li className="flex items-center mb-2">
+            <span className="w-4.5 h-4.5 rounded-full bg-pink-200 mr-2"></span>
+            cat 1
+          </li>
+
+          <li className="flex items-center mb-2">
+            <span className="w-4.5 h-4.5 rounded-full bg-green-200 mr-2"></span>
+            cat 1
+          </li>
+        </ul>
+      </nav>
+
+
+      <div className="h-full w-[85%] bg-zinc-200"></div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

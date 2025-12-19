982c16a5add7bee41394536540981548537d4ea3
Tusshar <tushargawhade7@gmail.com>
2025-12-19 20:28:48 +0530

    context used

---
+++ src/utils/Context.jsx
@@ -0,0 +1,34 @@
+import axios from "./axios";
+import React, { createContext, useEffect, useState } from "react";
+
+
+export const ProductContext = createContext();
+
+function Context(props) {
+  const [products, setProducts] = useState(null);
+
+
+
+  const getproducts = async () => {
+    try{
+      const {data} = await axios("/products");
+      console.log(data);
+      setProducts(data)
+
+    }catch(error){
+      console.log(error);
+    }
+  };
+
+
+  useEffect(()=> {
+    getproducts();
+  },[]);
+
+
+  return <ProductContext.Provider value={[products, setProducts]}>
+    {props.children} 
+  </ProductContext.Provider>;
+}
+
+export default Context;

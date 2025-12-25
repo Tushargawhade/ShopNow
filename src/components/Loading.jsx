ab1e2075e1d0639dd3cc08b37fcb968af8c90ce0
Tusshar <tushargawhade7@gmail.com>
2025-12-25 18:17:37 +0530

    .

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function Loading() {
   return (
-    <div className='w-full h-full flex items-center justify-center'>
-        
-        <h1 className='text-6xl font-semibold'>Loading....</h1>
+    <div className="w-full h-full flex items-center justify-center">
+      <h1 className="text-6xl   font-bold">Loading....</h1>
     </div>
-  )
+  );
 }
 
-export default Loading
+export default Loading;
